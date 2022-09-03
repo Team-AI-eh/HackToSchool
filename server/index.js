fcb5d3026700542a1467c0f4420cd74591fedf6f
Albert <albertthuynh94@gmail.com>
2022-09-03 14:08:44 -0400

    Fixed reactrouter refreshing issue

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 const PORT = 3000;
 
 app.listen(PORT, () => {
