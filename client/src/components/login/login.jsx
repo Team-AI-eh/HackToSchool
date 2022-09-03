f80940e613cdc8c0a9ef0e39b0475799b0be3262
Anthony Cella <anthony.cella242@gmail.com>
2022-09-03 19:14:27 -0400

    Got stuff done

--- client/src/components/login/login.jsx
+++ client/src/components/login/login.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+import firebase from 'firebase/app';
+import firebaseui from 'firebaseui';
+
+const ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 export default function Login() {
   return (
-    <div>
+    <LoginComponent>
       <h1>Login Page!</h1>
-    </div>
+    </LoginComponent>
   );
 }
+
+const LoginComponent = styled.div`
+  display: flex;
+`;
