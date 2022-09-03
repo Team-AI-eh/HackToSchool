5e4a24c22fd89d37d36f702bf02a8fa043a7f762
Anthony Cella <anthony.cella242@gmail.com>
2022-09-03 12:22:18 -0400

    Set up workspace to work with on the components

---
+++ client/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Timer from './timer/timer';
+import Profile from './profile/profile';
+import Analytics from './analytics/analytics';
+import Login from './login/login';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
